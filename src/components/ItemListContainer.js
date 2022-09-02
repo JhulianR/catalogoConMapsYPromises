eeac788a3ed299052484fb69e0c0496fdc614f66
Julián Ramírez <jhulianrb@gmail.com>
2022-09-01 21:33:39 -0500

    Agregados componentes y un array con los productos

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react"
+import item from "../utils/item";
+import desafio from "../utils/promesa";
+
 export const ItemListContainer = () => {
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+        desafio(item)
+        .then(resultado => setProducts(resultado)) 
+        .catch(err => console.log(err))
+    }, [])
+
     return (
-    <header>
-        <h1> Hola Mundo!</h1>
-    </header>
-    )}
+    <>
+    {
+        products.map(item => (
+        <div className="Productos">
+        <h4>{item.nombre}</h4>
+        <p>{item.precio}</p>
+        <p>{item.descripcion}</p>
+        <img src={item.pictureUrl} alt=""/>
+        </div>
+        ))
+    }
+    </>
+    )
+
+}
 
